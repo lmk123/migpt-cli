7e9fa269f91f7b413d840bcffbb7c602363acc20
mica <me@limingkai.cn>
2024-06-27 18:32:07 +0800

    :sparkles: (options): 添加预定义的 ai 服务

--- packages/options/src/Ai.tsx
+++ packages/options/src/Ai.tsx
@@ -1,5 +1,7 @@
 import { Card, FormGroup, InputGroup } from '@blueprintjs/core'
 import { produce } from 'immer'
+import { ChooseAI, ais } from './components/ChooseAI'
+import { useState } from 'react'
 
 type AiConfig = {
   OPENAI_API_KEY?: string
@@ -14,16 +16,60 @@ export function Ai(props: {
 }) {
   const { config, onChange } = props
 
+  const [selectedAIName, setSelectedAIName] = useState<string | null>(null)
+
+  const selectedAI = ais.find((ai) => ai.name === selectedAIName)
+
   return (
     <Card>
-      <p>
-        <a href="https://migptgui.com/docs/apply/" target={'_blank'}>
-          点击查看 AI 服务申请教程
-        </a>
-      </p>
+      <FormGroup
+        inline
+        label={'选择 AI 服务'}
+        helperText={
+          <>
+            根据你选择的服务，将会自动填写部分 AI 配置。
+            <a href="https://migptgui.com/docs/apply/" target={'_blank'}>
+              查看说明
+            </a>
+          </>
+        }
+      >
+        <ChooseAI
+          value={selectedAIName}
+          onChange={(name) => {
+            setSelectedAIName(name)
+
+            if (name == null) {
+              if (config) {
+                const newState = produce(config, (draft) => {
+                  if (draft) {
+                    draft.OPENAI_BASE_URL = undefined
+                    draft.OPENAI_API_KEY = undefined
+                    draft.OPENAI_MODEL = undefined
+                  }
+                })
+                onChange(newState!)
+              }
+              return
+            }
+
+            const ai = ais.find((ai) => ai.name === name)
+            if (ai) {
+              const newState = produce(config, (draft) => {
+                const c = draft || {}
+                c.OPENAI_BASE_URL = ai.urlAPI
+                c.OPENAI_MODEL = ai.models?.[0]
+                return c
+              })
+              onChange(newState!)
+            }
+          }}
+        ></ChooseAI>
+      </FormGroup>
       <FormGroup label={'接口地址'} inline>
         <InputGroup
           required
+          disabled={selectedAI != null}
           type={'url'}
           value={config?.OPENAI_BASE_URL || ''}
           onValueChange={(newVal) => {
@@ -45,7 +91,19 @@ export function Ai(props: {
           }
         />
       </FormGroup>
-      <FormGroup label={'密钥'} inline>
+      <FormGroup
+        label={'密钥'}
+        inline
+        helperText={
+          selectedAI ? (
+            <>
+              <a href={selectedAI.urlDocs} target={'_blank'}>
+                点击查看申请步骤
+              </a>
+            </>
+          ) : undefined
+        }
+      >
         <InputGroup
           required
           value={config?.OPENAI_API_KEY || ''}
@@ -61,7 +119,52 @@ export function Ai(props: {
           }}
         />
       </FormGroup>
-      <FormGroup label={'模型'} inline>
+      <FormGroup
+        label={'模型'}
+        inline
+        helperText={
+          selectedAI ? (
+            <>
+              {selectedAI.models && selectedAI.models.length > 1 && (
+                <>
+                  常用模型：
+                  {selectedAI.models.map((model, i) => (
+                    <>
+                      <a
+                        onClick={() => {
+                          const newState = produce(config, (draft) => {
+                            if (draft) {
+                              draft.OPENAI_MODEL = model
+                            } else {
+                              return { OPENAI_MODEL: model }
+                            }
+                          })
+                          onChange(newState!)
+                        }}
+                      >
+                        {model}
+                      </a>
+                      {i === selectedAI.models!.length - 1 ? '' : '、'}
+                    </>
+                  ))}
+                </>
+              )}
+
+              {selectedAI.name === '豆包' && (
+                <>
+                  豆包需要填写接入点 ID，
+                  <a
+                    href="https://migptgui.com/docs/apply/doubao#create-endpoint"
+                    target={'_blank'}
+                  >
+                    查看说明
+                  </a>
+                </>
+              )}
+            </>
+          ) : undefined
+        }
+      >
         <InputGroup
           required
           value={config?.OPENAI_MODEL || ''}
@@ -77,22 +180,25 @@ export function Ai(props: {
           }}
         />
       </FormGroup>
-      <FormGroup label={'Azure 密钥'} inline>
-        <InputGroup
-          placeholder={'不使用 Azure OpenAI 服务请留空'}
-          value={config?.AZURE_OPENAI_API_KEY || ''}
-          onValueChange={(newVal) => {
-            const newState = produce(config, (draft) => {
-              if (draft) {
-                draft.AZURE_OPENAI_API_KEY = newVal
-              } else {
-                return { AZURE_OPENAI_API_KEY: newVal }
-              }
-            })
-            onChange(newState!)
-          }}
-        />
-      </FormGroup>
+
+      {!selectedAIName && (
+        <FormGroup label={'Azure 密钥'} inline>
+          <InputGroup
+            placeholder={'不使用 Azure OpenAI 服务请留空'}
+            value={config?.AZURE_OPENAI_API_KEY || ''}
+            onValueChange={(newVal) => {
+              const newState = produce(config, (draft) => {
+                if (draft) {
+                  draft.AZURE_OPENAI_API_KEY = newVal
+                } else {
+                  return { AZURE_OPENAI_API_KEY: newVal }
+                }
+              })
+              onChange(newState!)
+            }}
+          />
+        </FormGroup>
+      )}
     </Card>
   )
 }
---
+++ packages/options/src/components/ChooseAI.tsx
@@ -0,0 +1,79 @@
+import { HTMLSelect } from '@blueprintjs/core'
+
+interface AIInfo {
+  name: string
+  urlDocs: string
+  urlAPI: string
+  models?: string[]
+}
+
+export const ais: AIInfo[] = [
+  {
+    name: 'OpenAI (ChatGPT)',
+    urlDocs: 'https://migptgui.com/docs/apply/chatgpt',
+    urlAPI: 'https://api.openai.com/v1',
+    models: ['gpt-3.5-turbo', 'gpt-4o'],
+  },
+  {
+    name: '智谱 AI',
+    urlDocs: 'https://migptgui.com/docs/apply/zhipu',
+    urlAPI: 'https://open.bigmodel.cn/api/paas/v4',
+    models: ['glm-4-flash', 'glm-4-air', 'glm-4-airx'],
+  },
+  {
+    name: '豆包',
+    urlDocs: 'https://migptgui.com/docs/apply/doubao',
+    urlAPI: 'https://ark.cn-beijing.volces.com/api/v3',
+  },
+  {
+    name: '通义千问',
+    urlDocs: 'https://migptgui.com/docs/apply/tongyi',
+    urlAPI: 'https://open.bigmodel.cn/api/paas/v4',
+    models: ['qwen-long', 'qwen-turbo', 'qwen-plus'],
+  },
+  {
+    name: 'DeepSeek',
+    urlDocs: 'https://migptgui.com/docs/apply/deepseek',
+    urlAPI: 'https://api.deepseek.com',
+    models: ['deepseek-chat'],
+  },
+  {
+    name: '零一万物（万知）',
+    urlDocs: 'https://migptgui.com/docs/apply/lingyi',
+    urlAPI: 'https://api.lingyiwanwu.com/v1',
+    models: ['yi-medium', 'yi-large-turbo', 'yi-large'],
+  },
+  {
+    name: '百川智能（百小应）',
+    urlDocs: 'https://migptgui.com/docs/apply/baichuan',
+    urlAPI: 'https://api.baichuan-ai.com/v1',
+    models: ['baichuan2-turbo', 'baichuan3-turbo', 'baichuan4'],
+  },
+  {
+    name: 'Moonshot (Kimi)',
+    urlDocs: 'https://migptgui.com/docs/apply/moonshot',
+    urlAPI: 'https://api.moonshot.cn/v1',
+    models: ['moonshot-v1-8k'],
+  },
+]
+
+export function ChooseAI(props: {
+  value: string | null
+  onChange: (name: string | null) => void
+}) {
+  return (
+    <HTMLSelect
+      value={props.value || ''}
+      onChange={(e) => {
+        props.onChange(e.target.value || null)
+      }}
+    >
+      <option value="">自定义</option>
+      {ais.map((info) => (
+        <option key={info.name} value={info.name}>
+          {info.name}
+        </option>
+      ))}
+    </HTMLSelect>
+  )
+}
