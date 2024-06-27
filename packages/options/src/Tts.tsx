25b6ae0bade5009f923b68331cefb39b63951d45
mica <me@limingkai.cn>
2024-06-27 17:47:25 +0800

    :sparkles: (options): 提示音效默认隐藏，且字段改为中文

--- packages/options/src/Tts.tsx
+++ packages/options/src/Tts.tsx
@@ -1,13 +1,13 @@
 import {
   Card,
   FormGroup,
-  H5,
   InputGroup,
   Radio,
   RadioGroup,
 } from '@blueprintjs/core'
 import { produce } from 'immer'
 import { MultiInput } from './components/MultiInput'
+import { useState } from 'react'
 
 interface TtsConfig {
   speaker: {
@@ -28,6 +28,8 @@ export function Tts(props: {
   onChange: (config: TtsConfig) => void
 }) {
   const { config, onChange } = props
+
+  const [tipTTS, setTipTTS] = useState<string>('default')
 
   return (
     <div className={'tw-space-y-4'}>
@@ -90,63 +92,102 @@ export function Tts(props: {
         )}
       </Card>
       <Card>
-        <H5>提示音</H5>
-        <FormGroup label={'AUDIO_SILENT'} inline>
-          <InputGroup
-            value={config.env?.AUDIO_SILENT || ''}
-            onValueChange={(newVal) => {
-              const newState = produce(config, (draft) => {
-                if (!draft.env) {
-                  draft.env = {}
-                }
-                draft.env.AUDIO_SILENT = newVal
-              })
-              onChange(newState)
+        <FormGroup label={'提示音效'} inline>
+          <RadioGroup
+            className={'tw-mb-0'}
+            inline
+            selectedValue={tipTTS}
+            onChange={(event) => {
+              const value = event.currentTarget.value
+              setTipTTS(value)
+
+              // 切换回 default 时，如果配置过提示音效则清空
+              if (value === 'default' && config.env) {
+                const newState = produce(config, (draft) => {
+                  Object.assign(draft.env!, {
+                    AUDIO_SILENT: undefined,
+                    AUDIO_BEEP: undefined,
+                    AUDIO_ACTIVE: undefined,
+                    AUDIO_ERROR: undefined,
+                  })
+                })
+                onChange(newState)
+              }
             }}
-          />
+          >
+            <Radio label="默认" value={'default'} />
+            <Radio label="自定义" value={'custom'} />
+          </RadioGroup>
         </FormGroup>
-        <FormGroup label={'AUDIO_BEEP'} inline>
-          <InputGroup
-            value={config.env?.AUDIO_BEEP || ''}
-            onValueChange={(newVal) => {
-              const newState = produce(config, (draft) => {
-                if (!draft.env) {
-                  draft.env = {}
-                }
-                draft.env.AUDIO_BEEP = newVal
-              })
-              onChange(newState)
-            }}
-          />
-        </FormGroup>
-        <FormGroup label={'AUDIO_ACTIVE'} inline>
-          <InputGroup
-            value={config.env?.AUDIO_ACTIVE || ''}
-            onValueChange={(newVal) => {
-              const newState = produce(config, (draft) => {
-                if (!draft.env) {
-                  draft.env = {}
-                }
-                draft.env.AUDIO_ACTIVE = newVal
-              })
-              onChange(newState)
-            }}
-          />
-        </FormGroup>
-        <FormGroup label={'AUDIO_ERROR'} inline>
-          <InputGroup
-            value={config.env?.AUDIO_ERROR || ''}
-            onValueChange={(newVal) => {
-              const newState = produce(config, (draft) => {
-                if (!draft.env) {
-                  draft.env = {}
-                }
-                draft.env.AUDIO_ERROR = newVal
-              })
-              onChange(newState)
-            }}
-          />
-        </FormGroup>
+
+        {tipTTS === 'custom' && (
+          <>
+            {/*AUDIO_SILENT*/}
+            <FormGroup label={'静音音频'} inline>
+              <InputGroup
+                type={'url'}
+                value={config.env?.AUDIO_SILENT || ''}
+                onValueChange={(newVal) => {
+                  const newState = produce(config, (draft) => {
+                    if (!draft.env) {
+                      draft.env = {}
+                    }
+                    draft.env.AUDIO_SILENT = newVal
+                  })
+                  onChange(newState)
+                }}
+              />
+            </FormGroup>
+            {/*AUDIO_BEEP*/}
+            <FormGroup label={'默认提示音'} inline>
+              <InputGroup
+                type={'url'}
+                value={config.env?.AUDIO_BEEP || ''}
+                onValueChange={(newVal) => {
+                  const newState = produce(config, (draft) => {
+                    if (!draft.env) {
+                      draft.env = {}
+                    }
+                    draft.env.AUDIO_BEEP = newVal
+                  })
+                  onChange(newState)
+                }}
+              />
+            </FormGroup>
+            {/*AUDIO_ACTIVE*/}
+            <FormGroup label={'唤醒提示音'} inline>
+              <InputGroup
+                type={'url'}
+                value={config.env?.AUDIO_ACTIVE || ''}
+                onValueChange={(newVal) => {
+                  const newState = produce(config, (draft) => {
+                    if (!draft.env) {
+                      draft.env = {}
+                    }
+                    draft.env.AUDIO_ACTIVE = newVal
+                  })
+                  onChange(newState)
+                }}
+              />
+            </FormGroup>
+            {/*AUDIO_ERROR*/}
+            <FormGroup label={'出错提示音'} inline>
+              <InputGroup
+                type={'url'}
+                value={config.env?.AUDIO_ERROR || ''}
+                onValueChange={(newVal) => {
+                  const newState = produce(config, (draft) => {
+                    if (!draft.env) {
+                      draft.env = {}
+                    }
+                    draft.env.AUDIO_ERROR = newVal
+                  })
+                  onChange(newState)
+                }}
+              />
+            </FormGroup>
+          </>
+        )}
       </Card>
     </div>
   )
